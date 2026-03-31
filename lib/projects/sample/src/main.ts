4684be77227375e0d1f39c173fc9cf95ee967c2f
times29 <11613026+times29@users.noreply.github.com>
2026-03-31 19:44:30 +0200

    Angular 21 (#36)
    * Update to Angular 21
    
    * Use Angular 21 concepts

--- lib/projects/sample/src/main.ts
+++ lib/projects/sample/src/main.ts
@@ -1,12 +1,11 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
-if (environment.production) {
-  enableProdMode();
-}
+if (environment.production) enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(), provideZoneChangeDetection()],
+}).catch((err) => console.error(err));
